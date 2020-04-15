aa3b84905f5371f2a34c94bfcc101fd21c27045b
llengleng <1132266959@qq.com>
2020-04-15 09:15:56 +0800

    init project

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from './element-ui'
+import '/element-ui@1.4/lib/theme-default/index.css'
 
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
